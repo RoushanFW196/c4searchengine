b51f730d46d4bd0e1bcc746d940a15db401faf17
RoushanFW196 <roushankumar6338@gmail.com>
2022-02-21 15:05:38 +0530

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import {Search} from "./components/Seacrh"
 import './App.css';
-
+import { Seacrhpage } from "./components/Searchpage";
+import {Routes, Route} from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+  
+    <Routes>
+      <Route path="/" element={<Search/>}/>
+      <Route path="/search" element={<Seacrhpage/>}/>
+    </Routes>
     </div>
   );
 }
