5855cae7ee3a52e8b76a24b6def14822266a7027
RoushanFW196 <roushankumar6338@gmail.com>
2022-02-21 17:00:56 +0530

    updating searchdetail

--- src/components/Searchdetailpage.jsx
+++ src/components/Searchdetailpage.jsx
@@ -8,7 +8,7 @@ export const Searchdetail=()=>{
     const[data,setData]=useState([]);
     console.log(data)
     const {id}=useParams()
-    console.log(id)
+    
     useEffect(()=>{
      
       getData();
@@ -16,16 +16,28 @@ export const Searchdetail=()=>{
 
 
    const getData=()=>{
-       fetch(`https://fast-reef-22226.herokuapp.com/data`).then((d)=>d.json()).then((res)=>setData(res))
-   }
+       fetch("https://fast-reef-22226.herokuapp.com/data").then((d)=>d.json()).then((res)=>setData(res))
+   };
 
 
 
     return(
-        {data.filter((e,id)=>{
-          if (e.id === id){
-              
-          }
-        })}
+        <>
+            {data.filter((e,id)=>{
+                if(e.id==id){
+                    console.log(id)
+                    return(
+                        <div id="detailed-result">
+                         <div className="title">{e.title}</div>
+                             <div className="desc">{e.description}</div>
+                             <div className="author">{e.author}</div>
+                             <div className="creation-date">{e.creation_date}</div>
+                             <div className="explicit">{e.explicit}</div>
+                             <div className="quality">{e.quality}</div>
+                        </div>
+                    )
+                }
+            })}
+        </>
     )
 }
