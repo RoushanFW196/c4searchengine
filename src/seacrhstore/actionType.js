831e8c8088bcc852c8b2ecf1c6fe23b29bcfa93b
RoushanFW196 <roushankumar6338@gmail.com>
2022-02-21 15:21:57 +0530

    updating actions

---
+++ src/seacrhstore/actionType.js
@@ -0,0 +1,9 @@
+export const SORT_AZ="SORT_AZ";
+export const SORT_ZA="SORT_ZA";
+
+export const SORT_BY_DATE_ASC="SORT_BY_DATE(ASC)";
+export const SORT_BY_DATE_DES="SORT_BY_DATE(DES)";
+export const SORT_BY_Quality_ASC="SORT_BY_Quality(ASC)";
+export const SORT_BY_Quality_DES="SORT_BY_Quality(DES)";
+export const Filter_EXPLICIT="FILTER_EXPLICIT";
+export const SEARCHRESULT="SEARCHRESULT";
